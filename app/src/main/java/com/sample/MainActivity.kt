3308a17d8c53c4a775acbebb118c6c5a4aaea715
Koti <kotibabu529@gmail.com>
2019-06-25 10:34:42 -0400

    added the searchview hint

--- app/src/main/java/com/sample/MainActivity.kt
+++ app/src/main/java/com/sample/MainActivity.kt
@@ -2,7 +2,9 @@ package com.sample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.SearchView
 import android.view.Menu
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +15,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_search, menu)
+        val searchView = menu?.findItem(R.id.m_search)?.actionView as SearchView
+        searchView.queryHint = getString(R.string.search_title)
         return true
     }
 }
